9966ea94dae4daacdbaf16e41924b4b19e747016
Xiaoling Gao <xiagao@redhat.com>
2019-09-02 10:56:43 +0800

    dddd

---
+++ virtio-win-acceptance/scripts/virtio-win-mapping-check/diff-qsr.sh
@@ -0,0 +1,21 @@
+#win2k8
+diff mnt-iso/qemupciserial/2k8/x86/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/2k8/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+
+#win7,win2k8R2
+diff mnt-iso/qemupciserial/w7/x86/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/w7/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/2k8R2/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+                                                                                                                         
+#Win8,Win8.1,2012,2012R2
+diff mnt-iso/qemupciserial/w8/x86/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/w8.1/x86/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/w8/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/w8.1/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/2k12/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/2k12R2/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+
+#win10,2016
+diff mnt-iso/qemupciserial/w10/x86/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/w10/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
+diff mnt-iso/qemupciserial/2k16/amd64/qemupciserial.inf virtio-win-prewhql-0.1-$1/rhel/qemupciserial.inf
