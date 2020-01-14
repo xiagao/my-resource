46f0f60292dbfcd18f9840f8eb2c584ca55e39af
Xiaoling Gao <xiagao@redhat.com>
2020-01-14 15:38:11 +0800

    ddd

--- virtio-win-acceptance/scripts/virtio-win-mapping-check/diff-fwg.sh
+++
@@ -1,5 +0,0 @@
-
-#win10,2016
-diff mnt-iso/qemufwcfg/w10/x86/qemufwcfg.inf virtio-win-prewhql-0.1-$1/qemufwcfg.inf
-diff mnt-iso/qemufwcfg/w10/amd64/qemufwcfg.inf virtio-win-prewhql-0.1-$1/qemufwcfg.inf
-diff mnt-iso/qemufwcfg/2k16/amd64/qemufwcfg.inf virtio-win-prewhql-0.1-$1/qemufwcfg.inf
